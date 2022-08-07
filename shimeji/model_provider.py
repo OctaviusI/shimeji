346eb984cf0afd17e048755acb4888de1c933c59
OctaviusI <59071826+OctaviusI@users.noreply.github.com>
2022-08-06 22:20:15 -0400

    Update model_provider.py

--- shimeji/model_provider.py
+++ shimeji/model_provider.py
@@ -288,7 +288,7 @@ class Sukima_ModelProvider(ModelProvider):
                 'rep_p_range': args.sample_args.rep_p_range,
                 'rep_p_slope': args.sample_args.rep_p_slope,
                 'bad_words': args.sample_args.bad_words,
-                'logit_biases': [ob.toJSON() for ob in args.sample_args.logit_biases])  
+                'logit_biases': ([ob.toJSON() for ob in args.sample_args.logit_biases])  
             },
             'gen_args': {
                 'max_length': args.gen_args.max_length,
